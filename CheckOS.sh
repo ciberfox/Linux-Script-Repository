3e773bf9356a9920e9c4042c5e07c5e6791b5e4e
ciberfox <22546170+ciberfox@users.noreply.github.com>
2023-03-10 15:02:47 +0100

    Create CheckOS.sh
    Check SO version

---
+++ CheckOS.sh
@@ -0,0 +1,18 @@
+# Determine the operating system that is running the script
+checkOperatingSystem()
+{
+  operatingSystem="unknown"
+  os=`uname | tr [a-z] [A-Z]`
+  case "${os}" in
+    CYGWIN*)
+      operatingSystem="cygwin"
+      ;;
+    LINUX*)
+      operatingSystem="linux"
+      ;;
+    MINGW*)
+      operatingSystem="mingw"
+      ;;
+  esac
+  echo "operatingSystem=$operatingSystem (used to check for Cygwin and filemode compatibility)"
+}
